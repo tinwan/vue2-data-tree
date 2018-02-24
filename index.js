8bd960dc38b3391d437c8b057afee5470acc61d0
tinwan <tianyun@nationsky.com>
2018-02-24 17:45:23 +0800

    Bug fixed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Component = require('./src/tree.vue');
 module.exports = Component;
 
 Component.install = Vue => Vue.component(Component.name, Component);
-Component.version = proccess.env.VERSION;
 
 if (typeof window !== 'undefined' && window.Vue) {
     window.Vue.use(Component);
